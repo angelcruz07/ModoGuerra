16d92ba70a85c866dff4e4111dcb40d3e3f49915
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-10-14 23:47:39 -0600

    feat: add metadata

---
+++ src/const/site-info.ts
@@ -0,0 +1,7 @@
+export const siteInfo = {
+  title: "Modo Guerra",
+  description:
+    "Activa el #ModoGuerra. El cuartel general para los compas de El Temach. Artículos y guías de desarrollo personal para forjar tu mejor versión. Sin pretextos, solo resultados.",
+  image:
+    "https://res.cloudinary.com/dlklqucye/image/upload/v1760310621/modoguerra/posts/all/image_vkcqln.png",
+};
